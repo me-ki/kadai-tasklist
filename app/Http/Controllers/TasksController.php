6fbdd11ba85247fbd1f07d38f290b1cc1807be69
me.ki <walk.beaute@gmail.com>
2021-08-29 05:30:07 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -42,7 +42,13 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'status' => 'required|max:10',
+            'content' => 'required|max:255',
+        ]);
+        
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
@@ -88,8 +94,14 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $request->validate([
+            'status' => 'required|max:10',
+            'content' => 'required|max:255',
+        ]);
+        
         $task = Task::findOrFail($id);
         
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
