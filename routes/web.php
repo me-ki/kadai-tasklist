394a6e60450c035d49ba474fce0683dd53e36bb9
me.ki <walk.beaute@gmail.com>
2021-09-11 10:27:17 +0000

    create_login_check

--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,5 @@ Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
 Route::group(['middleware' => ['auth']], function () {
-     Route::resource('tasks', 'TasksController', ['only' => ['store', 'update', 'destroy']]);   
+     Route::resource('tasks', 'TasksController', ['only' => ['create', 'store', 'update', 'destroy']]);   
 });
